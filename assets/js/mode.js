1f04a558cde753e3dd5b60b2444826e5fd1b11d8
melihsahtulek <melihsahtulek@gmail.com>
2022-01-30 23:12:58 +0300

    fixed styles and isues. imp. dark-light mode

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -1,5 +1,32 @@
 /*
 
   Light/Dark Switch By melihsahtulek
+  
 
 */
+
+window.addEventListener("load", () => {
+  const check = document.querySelector("#check");
+
+  const setBodyClass = () => document.body.classList.replace(!check.checked ? "dark" : "light", check.checked ? "dark" : "light");
+
+  if (localStorage.getItem("mode")) {
+    if (localStorage.getItem("mode") === "dark") {
+      document.body.classList.add("dark");
+      check.checked = true;
+    } else {
+      document.body.classList.add("light");
+      check.checked = false;
+    }
+    setBodyClass();
+  } else {
+    localStorage.setItem("mode", "light");
+    document.body.classList.add("light");
+    setBodyClass();
+  }
+
+  check.addEventListener("change", (e) => {
+    localStorage.setItem("mode", e.target.checked ? "dark" : "light");
+    setBodyClass();
+  });
+});
